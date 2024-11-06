8cb42c7acd3cff1dfed499f54e10a4f706f8b81e
sub's <subs@subsui-MacBookPro.local>
2024-11-06 17:15:01 +0900

    Remove large file

--- js/common.js
+++ js/common.js
@@ -1,41 +1,51 @@
 let onePlay = false;
 window.onload = function(){
     const _conWrapper = document.querySelector(".content-wrapper");
-    const _con10 = document.querySelector(".content-area.con10 .innerWrapper");
+    const _con9 = document.querySelector(".content-area.con9 .innerWrapper");
     const _popMovie = document.querySelector(".modal-pop.movie");
     const _popImg = document.querySelector(".modal-pop.img");
     const _popCloseBtMovie = _popMovie.querySelector(".clsoe-button");
     const _popCloseBtImg = _popImg.querySelector(".clsoe-button");
+    const _rnbMenu = document.querySelector("#rnbMenu");
+    const _rnb = document.querySelector("#rnb");
+    const _rnbClose = document.querySelector(".rnb-close-btn");
+    const _modalOpenBt = document.querySelector(".web-modal-open");
     let popupChecked = true;
     let delayTime = false;
     let delayTimer = null;
     let checkedNum = 0;
     let checkedEvent = false;
+    let touchChecked = 0;
 
-    setCon1();
-    setCon2();
-    setCon3();
-    setCon4();
-    setCon5();
-    setCon6();
-    setCon7();
-    setCon8();
+    setTimeout(()=>{
+        _popMovie.classList.add("open");
+        setTimeout(()=>{
+            document.querySelector(".modal-video").play();
+            document.querySelector(".modal-video").addEventListener("ended",()=>{
+                console.log("popupChecked : ",popupChecked)
+                setTimeout(()=>{
+                    if(popupChecked){
+                        _popMovie.classList.remove("open");
+                        _popImg.classList.add("open");
+                    }
+                },500)
+            })
+        },3000)
+    },500)
 
-    // setTimeout(()=>{
-    //     _popMovie.classList.add("open");
-    //     setTimeout(()=>{
-    //         document.querySelector(".modal-video").play();
-    //         document.querySelector(".modal-video").addEventListener("ended",()=>{
-    //             console.log("popupChecked : ",popupChecked)
-    //             setTimeout(()=>{
-    //                 if(popupChecked){
-    //                     _popMovie.classList.remove("open");
-    //                     _popImg.classList.add("open");
-    //                 }
-    //             },500)
-    //         })
-    //     },3000)
-    // },500)
+    _modalOpenBt.addEventListener("click",()=>{
+            _popMovie.classList.add("open");
+            document.querySelector(".modal-video").play();
+    })
+
+    _rnbMenu.addEventListener("click",()=>{
+        _rnb.classList.add("open");
+    })
+    _rnbClose.addEventListener("click",()=>{
+        _rnb.classList.remove("open");
+
+    })
+
     _popCloseBtMovie.addEventListener("click",()=>{
         _popMovie.classList.remove("open");
         document.querySelector(".modal-video").pause();
@@ -49,46 +59,67 @@ window.onload = function(){
     _popCloseBtImg.addEventListener("click",()=>{
         _popImg.classList.remove("open");
     })
-    _con10.addEventListener("scroll",(ev)=>{
+    const scrollEv = (ev)=>{
         ev.preventDefault();
         const scrollTop = ev.currentTarget.scrollTop;
-        const _books = _conWrapper.querySelectorAll(".con10 .book-box-wrapper.book2 .book-box, .con10 .book-box-wrapper.book3 .book-box");
-        const _texts = _conWrapper.querySelectorAll(".con10 .book-box-wrapper.book2 .book-text-con, .con10 .book-box-wrapper.book3 .book-text-con");
-        const _video = _conWrapper.querySelector(".con10 .beyond-centum-video");
+        const _firstBooks = _conWrapper.querySelector(".con9 .book-box-wrapper.book1 .book-box");
+        const _books = _conWrapper.querySelectorAll(".con9 .book-box-wrapper.book2 .book-box, .con9 .book-box-wrapper.book3 .book-box");
+        const _texts = _conWrapper.querySelectorAll(".con9 .book-box-wrapper.book2 .book-text-con, .con9 .book-box-wrapper.book3 .book-text-con");
+        const _video = _conWrapper.querySelector(".con9 .beyond-centum-video");
+        const _both = _conWrapper.querySelector(".con9 .beyond-centum-bottom-text");
+        const _bothLogo = _conWrapper.querySelector(".con9 .beyond-centum-bottom-logo");
         const wt = window.innerHeight;
+        const firstBookTop = _firstBooks.getBoundingClientRect().top;
+        const bothTop = _both.getBoundingClientRect().top;
+        _rnb.classList.remove("open");
+        if(bothTop - wt <= -50){
+            _both.querySelector(".beyond-centum-bottom-text-wrapper").classList.add("active")
+            _bothLogo.classList.add("active")
+        }
+        if(firstBookTop - wt <= -110){
+            _firstBooks.parentNode.classList.add("active")
+        }
         _books.forEach((b,i)=>{
             const t = b.getBoundingClientRect().top;
-            if(i === 0 && t - wt <= -250){
+            if(i === 0 && t - wt <= -110){
                 b.parentNode.classList.add("active");
                 _texts[i].classList.add("active");
             }
         })
         _texts.forEach((target,idx)=>{
             const t = target.getBoundingClientRect().top;
-            if(idx === 1 && t - wt <= -100){
-                target.classList.add("active");
-                _books[idx].parentNode.classList.add("active");
+            if(idx === 1 && t - wt <= -110){
+                setTimeout(()=>{
+                    target.classList.add("active");
+                    _books[idx].parentNode.classList.add("active");
+                },500)
             }
         })
         const videoTop = _video.getBoundingClientRect().top;
-        if(wt - videoTop >= 200){
+        if(wt - videoTop >= 150){
             onePlay = true;
-            _con10.querySelector("video").play();
+            document.querySelector(".beyond-centum-video video").play()
         }
+    }
+    _con9.addEventListener("scroll",(ev)=>{
+        scrollEv(ev);
     })
-    _conWrapper.addEventListener("wheel",(ev)=>{
+    const controllEv = (ev)=>{
+        _rnb.classList.remove("open");
         if(delayTime || checkedNum > 1){
             return;
         }
         delayTime = true;
         checkedNum += 1;
-        const dy = ev.deltaY;
+        const dy = (ev.deltaY)?ev.deltaY:touchChecked - ev.changedTouches[0].screenY;
         const checked = getCheckedCurrentPosition();
         checkedEvent = true;
+        touchChecked = 0;
         if(dy > 0){
-            if(checked !== "w11") event.preventDefault();
+            if(checked !== "w10") event.preventDefault();
             if(checked == null){
                 setCon1();
+                // if(!ev.deltaY) setCon2();
             }else if(checked == "w1"){
                 setCon2();
             }else if(checked == "w2"){
@@ -105,15 +136,13 @@ window.onload = function(){
                 setCon9();
             }else if(checked == "w9"){
                 setCon10();
-            }else if(checked == "w10"){
-                setCon11();
             }else{
                 checkedEvent = false;
                 delayTime = false;
                 checkedNum = 0;
             }
-        }else{
-            if(checked !== "w11" || _con10.scrollTop <= 0){
+        }else if(dy < 0){
+            if(checked !== "w10" || _con9.scrollTop <= 0){
                 event.preventDefault();
             }
             if(checked == "w1"){
@@ -121,6 +150,7 @@ window.onload = function(){
                 resetCon1();
             }else if(checked == "w2"){
                 resetCon2();
+                // if(!ev.deltaY) resetCon1();
             }else if(checked == "w3"){
                 resetCon3();
             }else if(checked == "w4"){
@@ -136,10 +166,8 @@ window.onload = function(){
                 resetCon8();
             }else if(checked == "w9"){
                 resetCon9();
-            }else if(checked == "w10"){
+            }else if(checked == "w10" && _con9.scrollTop <= 0){
                 resetCon10();
-            }else if(checked == "w11" && _con10.scrollTop <= 0){
-                resetCon11();
             }else{
                 checkedEvent = false;
                 delayTime = false;
@@ -152,20 +180,32 @@ window.onload = function(){
             delayTime = false;
             checkedNum = 0;
         },1500);
+    }
+    _conWrapper.addEventListener("touchstart",(ev)=>{
+        touchChecked = ev.changedTouches[0].screenY;
+    })
+    _conWrapper.addEventListener("touchend",(ev)=>{
+        controllEv(ev);
+    })
+    _conWrapper.addEventListener("wheel",(ev)=>{
+        controllEv(ev);
     })
 
-    document.querySelector(".top-bottom").addEventListener("click",()=>{
-        resetCon1();
-        resetCon2();
-        resetCon3();
-        resetCon4();
-        resetCon5();
-        resetCon6();
-        resetCon7();
-        resetCon8();
-        resetCon9();
-        resetCon10();
-        resetCon11();
+    document.querySelectorAll(".top-bottom").forEach((b,i)=>{
+        b.addEventListener("click",()=>{
+            resetCon1();
+            resetCon2();
+            resetCon3();
+            resetCon4();
+            resetCon5();
+            resetCon6();
+            resetCon7();
+            resetCon8();
+            resetCon9();
+            resetCon10();
+            delayTime = false;
+            checkedNum = 0;
+        })
     })
 }
 
@@ -434,9 +474,13 @@ const setCon10 = ()=>{
     const _conWrapper = document.querySelector(".content-wrapper");
     _conWrapper.classList.add("w10");
     const _movieWrapper = _conWrapper.querySelector(".con9 .movie-box");
+    const _con9 = document.querySelector(".content-area.con9 .innerWrapper");
     setTimeout(()=>{
         // _movieWrapper.classList.add("active");
         _movieWrapper.querySelector("video").play();
+        const ev = document.createEvent("HTMLEvents");
+        ev.initEvent("scroll",true,true);
+        _con9.dispatchEvent(ev);
     },1500)
 }
 const resetCon10 = ()=>{
@@ -447,6 +491,28 @@ const resetCon10 = ()=>{
         _movieWrapper.classList.remove("active");
         _movieWrapper.querySelector("video").pause();
     },800)
+    const _books = _conWrapper.querySelectorAll(".con9 .book-box-wrapper");
+    const _texts = _conWrapper.querySelectorAll(".con9 .book-box-wrapper .book-text-con");
+    const _con9 = document.querySelector(".content-area.con9 .innerWrapper");
+
+
+    const _both = _conWrapper.querySelector(".con9 .beyond-centum-bottom-text");
+    const _bothLogo = _conWrapper.querySelector(".con9 .beyond-centum-bottom-logo");
+
+    setTimeout(()=>{
+        _books.forEach((_b,idx)=>{
+            _b.classList.remove("active");
+        })
+        _texts.forEach((_b,idx)=>{
+            _b.classList.remove("active");
+        })
+        _con9.scrollTop = 0;
+        onePlay = false;
+        document.querySelector(".beyond-centum-bottom-text-wrapper").classList.remove("active");
+        _both.classList.remove("active");
+        _bothLogo.classList.remove("active");
+    },800)
+    document.querySelector(".beyond-centum-video video").pause();
 }
 const setCon11 = ()=>{
     const _conWrapper = document.querySelector(".content-wrapper");
@@ -461,7 +527,7 @@ const resetCon11 = ()=>{
     _conWrapper.classList.remove("w11");
     const _books = _conWrapper.querySelectorAll(".con10 .book-box-wrapper");
     const _texts = _conWrapper.querySelectorAll(".con10 .book-box-wrapper .book-text-con");
-    const _con10 = document.querySelector(".content-area.con10 .innerWrapper");
+    const _con9 = document.querySelector(".content-area.con10 .innerWrapper");
     setTimeout(()=>{
         _books.forEach((_b,idx)=>{
             _b.classList.remove("active");
@@ -469,9 +535,9 @@ const resetCon11 = ()=>{
         _texts.forEach((_b,idx)=>{
             _b.classList.remove("active");
         })
-        _con10.scrollTop = 0;
+        _con9.scrollTop = 0;
         onePlay = false;
-        _con10.querySelector("video").pause();
+        _con9.querySelector("video").pause();
     },800)
 }
 
